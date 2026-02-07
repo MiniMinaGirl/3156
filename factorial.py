4e3e5a65cf597cd500cdbdf3316f11cd15dbac4c
MiniMinaGirl <minaraismayilova13@gmail.com>
2026-02-07 23:07:49 +0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -5,7 +5,10 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
+        n -= 1  # <-- Xətanın həlli: n hər dəfə 1 vahid azalmalıdır
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        f = factorial(int(sys.argv[1]))
+        print(f)
